83c07650e7b697e33140ce4d335b6f1536fc54a4
Nereu Costa Nogueira <nereu.nogueira.ext@vr.com.br>
2024-02-15 21:05:07 -0300

    login

---
+++ specs/login/login.js
@@ -0,0 +1,8 @@
+import login from '../../pages/login/login'
+
+fixture('Getting Started')
+    .page('https://app-portalautomatizadosec4-prd.azurewebsites.net/');
+
+test('Login com sucesso', async t => {
+    
+});
